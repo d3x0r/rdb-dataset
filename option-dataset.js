ed163bbe515da1e1a6ac1a595c834224a1cfbf26
d3x0r <d3x0r@no-reply.github.com>
2018-02-02 21:48:20 -0800

    Merge some work on active dataset

--- option-dataset.js
+++ option-dataset.js
@@ -1,32 +1,31 @@
+var rdb = require( "./rdb-dataset.js" );
 
-var optiondb = DataSet();
+var optiondb = rdb.DataSet();
 optiondb.Table( "option_name" );
-var col = optiondb.option_name.Column( { type: "guid" } );
-var namecol = optiondb.option_name.Column( { name : "option_name", type: "string" } )
-optiondb.option_name.primaryKey = col;
-optiondb.option_name.uniqueKey = namecol;
-
+var col = optiondb.option_name.Column( { type: "guid", primaryKey : true, autoKey : ()=>Math.random() } );
+var namecol = optiondb.option_name.Column( { isName : true, type: "string", unique : true } )
 
 optiondb.Table( "option_map" );
-var idcol = optiondb.option_map.Column( { name : "option_id", type : "guid" } );
-var nameidx = optiondb.option_map.Column( { name : "option_name_id", type : "guid" ) );
-var parentidcol = optiondb.option_map.Column( { name : "parent_option_id", type : "guid" } );
-optiondb.option_map.index = nameidx;
-optiondb.option_map.primaryKey = idcol;
-
-optiondb.ForeignKey( optiondb.option_map, ["parent_option_id"],  optiondb.option_map, ["option_id"] );
-optiondb.ForeignKey( optiondb.option_name, ["option_name_id"], optiondb.option_map, ["option_name_id"] );
-
+var idcol = optiondb.option_map.Column( { name : "option_id", type : "guid", primaryKey : true, autoKey : ()=>Math.random() } );
+var nameidx = optiondb.option_map.Column( { name : "option_name_id", type : "guid", indexed : true, foreign: { table : "option_name", column: "option_name_id", onUpdate:rdb.constants.Cascade, onDelete:rdb.constants.Cascade} } );
+var parentidcol = optiondb.option_map.Column( { name : "parent_option_id", type : "guid", foreign: { table : "option_map", column: "option_id", onUpdate:rdb.constants.Cascade, onDelete:rdb.constants.Cascade } } );
 
 optiondb.Table( "option_value" );
-optiondb.option_value.Column( { name : "option_id", type: "guid" } );
+optiondb.option_value.Column( { name : "option_id", type: "guid", foreign: { table : "option_map", column: "option_id", onUpdate:rdb.constants.Cascade, onDelete:rdb.constants.Cascade } } );
 optiondb.option_value.Column( { name : "number", type: "int" } );
-optiondb.option_value.index = optiondb.option_value.option_id;
-optiondb.ForeignKey( optiondb.option_map, ["option_id"], optiondb.option_value, ["option_id"] );
+
+console.log(" So then we have:", optiondb.option_map.getCreate() )
+console.log(" So then we have:", optiondb.option_name.getCreate() )
+console.log(" So then we have:", optiondb.option_value.getCreate() )
+
+var nameRow = optiondb.option_name.Row( { [namecol.name] : "first name" } );
+var optionRow = optiondb.option_map.Row( { option_id : "0000", parent_option_id : "0000", option_name_id: nameRow.option_name_id } );
 
 
+console.log( "then?", nameRow );
+console.log( "then?", optionRow );
 
-optiondb.prototype.getValue = function( path, default ) { 
+optiondb.getValue = function( path, Default ) { 
 	var option_path = path.split( '/' );
         
 }
--- rdb-dataset.js
+++ rdb-dataset.js
@@ -1,4 +1,48 @@
-"use strict";
+// Relational Data Base - Data Set 
+//
+// Provides mapping of related SQL tables.  
+// Table schema representation
+//
+// var rdb = rdb.DataSet();
+// rdb.Table( "tableName" );
+// rdb.tableName.Column( { ... Column Definition ... } );
+// rdb.Table( "Table Name" );  // not the preferred usage.
+// rdb["Table Name"].Column( { ... Column Definition ... } );  // not the preferred usage.
+//
+//
+//  Column Definition
+//   name : column name
+//   type : column type + precision
+//   defaultValue : default value
+//   allowNul : specifically allow NULL, else NOT NULL is built into table definition
+//   foreign : {        foreign key (single column) 
+//        table : table name this column references
+//        column : column name this column references
+//        onUpdate: constants.Delete .... 
+//        onDelete:
+//   }
+//   primaryKey : column is the primary key  (auto increment) of this table
+//   autoKey : (callback)  this is called when the value for a new row is needed.
+//   unique :  column should be unqiue
+//   
+//   
+//   
+
+
+const constants = {
+	NoAction : 1,
+	Cascade : 2,
+	SetNull : 3,
+	SetDefault : 4,
+	Restrict : 5,
+};
+
+module.exports = exports = {
+   constants : constants,
+   DataSet: DataSet,
+  DataTable : DataTable,
+  DataColumn : DataColumn,
+}
 
 	function singularize( string ) {
 		if( string.endsWith( "ies" ) )
@@ -9,32 +53,282 @@
         		return string.substr( 0, string.length-1 );
 		return string;
 	}
+        
 
-exports.Table = function ( tableName ) {
-	var tableObject = {
-    	name : tableName
-		, columns : []
-        , rows: []
-        , rowProto : {}
-        , Row : ( data ) => { var newRow = { data:data } }
-		, flags : { temporary : false, ifNotExist : false }
-		, keys : []
-		, constraints : []
-		, Column : ( def ) => {
-        	if( !def.hasOwnProperty( "name" ) )
-                	def.name = singularize( tableObject.name ) + "_id";
-                var newCol = { name : def.name
-					,type : def.type
-					, extra : def.extra
-					 ,default : null };
-				//Object.defineProperty( newRow, col.name
-				//		, { get : function() } );
-				tableObject.columns.push( newCol );
-                return newCol;
+function DataSet() {
+	var ds = {
+        	prefix : ""
+                , name : ""
+		, tables : []
+		, relations : []
+        	, Table : Table
+		, push : push
+		, Relation : Relation
         }
-    }
-    return tableObject;
+	function Relation( name, ) {
+		var relation = {
+			name : name, 
+			parent : { table : parentTable, column : parentColumn },
+			child : { table : childTable, column : childColumn },
+		}
+		this.relations.push( relation );
+		
+		return relation;
+	}
+        function Table( name ) {
+        	var table = DataTable( name );
+		return this.push( table );
+        }
+	function push( table ) {
+        	ds.tables.push( table );
+		ds[table.name] = table;
+                table.dataSet = ds;
+                return table;
+	}
+        return ds;
 }
+
+function newRow( table ) {
+	var row = Object.assign( {}, table.rowProto );
+	row.table = table;
+	//console.log( "NEW ROW" );
+	table.columns.forEach( (col,idx)=>{
+		var value = col.defaultValue
+		if( col.autoKey )
+			value = col.autoKey();
+		
+		//console.log( "For the record; value is:", value );
+		row._cells.push( DataCell( row, value ) );
+		//console.log( "new property: ", col.name );
+		Object.defineProperty( row, col.name, {
+			enumerable : true,
+			
+			get () {
+				//console.log( "THIS:", idx, row._cells[idx] );
+				return row._cells[idx].Value;
+			},
+			set (value) {
+				//console.log( "This should also be setting value...", idx, value );
+				row._cells[idx].Value = value;
+			}
+		} ); 
+		//row[col.name] = value;
+	} );
+	return row;
+}
+
+function mapHistory( row ) {
+	var fields = Object.keys( row );
+	var history = {};
+	//historyData.push( history );
+	fields.forEach( (field)=>{
+		history[field] = { values:[row[field]], current : 0 };
+	});
+	return row;
+}
+
+function DataCell( row, value ) {
+	return {
+		events : {},
+		on(event,handler) {
+			if( typeof handler === "function" ) {
+				if( event in events ) events[event].push( handler );
+				else events[event] = [handler];
+			} else {
+				if( event in events ) events[event]( handler );
+			}
+		},
+		row: row,
+		versions : 0,
+		history : [ value ],
+		value : value,
+		get Value() {
+			return this.value;
+		},
+		set Value(value) {
+			console.log( "Data Cell has data change" );
+			if( this.value === value ) return;
+			this.history.push( this.value = value );
+			this.on( "changed", value );
+		},
+		get versions() {
+			return this.history.length;
+		},
+		get Values() {
+			return this.history;
+		},
+		revert() {
+			if( this.history.length > 1 ) {
+				this.value = this.history[0];
+				this.history.length = 1;
+				this.on( "changed", value );
+			}
+		},
+		commit() {
+			if( this.history.length > 1 ) {
+				this.history[0] = this.value = this.history[this.history.length-1];
+				this.history.length = 1;
+				this.on( "changed", value );
+			}
+		}
+	}
+}
+
+function DataTable( tableName ) {
+	var historyData = [];
+        var rowData = []; // private variable for row data.
+	var tableObject;
+	if( tableName in this ) {
+		tableObject = this[tableName];
+		tableObject.auto = false;
+	} else
+		tableObject = { 
+        	prefix : ""
+        	, name : tableName
+		, dataSet : null
+		, get fullName() {
+			return this.dataSet.prefix + this.dataSet.name + this.prefix + this.name;
+		}
+		, auto : false  // this is true if it was created 
+        	, columns : []
+		, changedRows : []
+                , get rows() {
+			 return {
+				get value()  {
+					return rowData;
+				}
+			}
+		}
+                , rowProto : {
+			_rowState : 0,
+			_cells : [],
+			table : null,
+		}
+                , Row( data ) { 
+			var dataKeys = Object.keys( data );
+			var row = newRow( this );
+			dataKeys.forEach( field=> {
+				console.log( "Override old values with:", field, data[field] );
+				newRow[field] = data[field];
+			} );
+			rowData.push( row );
+			historyData.push( mapHistory(row) );
+			this.changedRows.push( row );
+			return row;			
+		}
+                , Column : function( def ) { 
+	        	if( def.isName )
+        	        	def.name = singularize( this.name ) + "_name";
+	        	else if( !def.hasOwnProperty( "name" ) )
+        	        	def.name = singularize( this.name ) + "_id";
+			if( !this.rowProto.table )
+				this.rowProto.table = this;
+
+			this.rowProto[def.name] = def.defaultValue;
+			var column = DataColumn( def );
+			this.columns.push( column );
+			this[def.name] = column;
+			if( def.foreign ) {
+				if( !def.foreign.table in this ) {
+					this.dataset.Table( def.foreign.table );
+					this.dataset[def.foreign.table].auto = true;
+					this.dataset[def.foreign.table].Column( { name : def.foreign.column, type : def.type } );
+				}
+				this.dataSet.Relation( def.foreign.name || ( def.foreign.table.name + "_has_" + this.table.name )
+					, this.dataSet[def.foreign.table], this.dataSet[def.foreign.table][def.foreign.column] 
+					, this, column
+				);
+			}
+			return column;
+		}
+		, Fill( db, condition ) {
+			rowData = db.do( "select * from [${this.name}]" + condition?"WHERE "+condition:"" );
+			historyData = rowData.map( mapHistory );
+			this.rows = rowData.map( (row)=>{
+				return row;
+			} );
+		}
+		, commit() {
+			this.changedRows.forEach( row => row.commit() );
+			this.changedRows.length = 0;
+		}
+		, reject() {
+			this.changedRows.forEach( row => row.reject() );
+			this.changedRows.length = 0;
+		}
+		, getCreate() {
+			var stmt = "CREATE TABLE [" + this.name + "](";
+			this.columns.forEach( (col,idx)=>{
+				var t = col.type;
+				if( t == 'guid' ) {
+					t = "char";
+				}
+				if( t == 'decimal' ) {
+					t = "float";
+				}
+				if( idx > 0 ) stmt += ",";
+				stmt += (col.name + " " + t 
+					+ ( ("defaultValue" in col)?" "  + col.defaultValue:"") 
+					+ (col.allowNull?"" : " NOT NULL" )
+					+ (col.primaryKey?" PRIMARY KEY":"" ) );
+				
+			} );
+			this.columns.forEach( (col,idx)=>{
+				if( col.foreign ) {
+					//if( col.foreign.
+					// some databases require constraint before foriegn key
+					// stmt += "CONSTRAINT '" + "SomeNameHere" + "' ";			
+					stmt += ",";
+					stmt += "FOREIGN ([" + col.name + "])REFERENCES [" + col.foreign.table + "]([" + col.foreign.column + "])";
+					switch( col.foreign.onDelete ) {
+					case constants.NoAction:
+						stmt += " ON DELETE NO ACTION";
+						break;
+					case constants.Cascade:
+						stmt += " ON DELETE CASCADE";
+						break;
+					case constants.SetNull:
+						stmt += " ON DELETE SET NULL";
+						break;
+					case constants.SetDefault:
+						stmt += " ON DELETE SET DEFAULT";
+						break;
+					case constants.Restrict:
+						stmt += " ON DELETE RESTRICT";
+						break;
+					}
+					switch( col.foreign.onUpdate ) {
+					case constants.NoAction:
+						stmt += " ON UPDATE NO ACTION";
+						break;
+					case constants.Cascade:
+						stmt += " ON UPDATE CASCADE";
+						break;
+					case constants.SetNull:
+						stmt += " ON UPDATE SET NULL";
+						break;
+					case constants.SetDefault:
+						stmt += " ON UPDATE SET DEFAULT";
+						break;
+					case constants.Restrict:
+						stmt += " ON UPDATE RESTRICT";
+						break;
+					}
+				}
+			} );
+			stmt += ")  -- extra";
+			return stmt;
+		}
+        }
+        return tableObject;
+}
+
+function DataColumn( coldef ) {
+	return Object.assign( {}, coldef );
+}
+
+
+
 
 
 
@@ -294,9 +588,30 @@ function AddConstraint( table, wordref )
 			if( wordref.word.text.toUpperCase() ===  ("CONFLICT" ) )
 			{
 				wordref.word = Next( wordref );
-				if( wordref.word.text.toUpperCase() === ( "REPLACE" ) )
+				if( StrCaseCmp( GetText(*word), WIDE( "REPLACE" ) ) == 0 )
 				{
-					wordref.word = Next( wordref );
+					//table->keys.key[table->keys.count-1].flags.uniqueResolution = UNIQRES_REPLACE;
+					(*word) = NEXTLINE( *word );
+				}
+				if( StrCaseCmp( GetText(*word), WIDE( "IGNORE" ) ) == 0 )
+				{
+					//table->keys.key[table->keys.count-1].flags.uniqueResolution = UNIQRES_IGNORE;
+					(*word) = NEXTLINE( *word );
+				}
+				if( StrCaseCmp( GetText(*word), WIDE( "FAIL" ) ) == 0 )
+				{
+					//table->keys.key[table->keys.count-1].flags.uniqueResolution = UNIQRES_FAIL;
+					(*word) = NEXTLINE( *word );
+				}
+				if( StrCaseCmp( GetText(*word), WIDE( "ABORT" ) ) == 0 )
+				{
+					//table->keys.key[table->keys.count-1].flags.uniqueResolution = UNIQRES_ABORT;
+					(*word) = NEXTLINE( *word );
+				}
+				if( StrCaseCmp( GetText(*word), WIDE( "ROLLBACK" ) ) == 0 )
+				{
+					//table->keys.key[table->keys.count-1].flags.uniqueResolution = UNIQRES_ROLLBACK;
+					(*word) = NEXTLINE( *word );
 				}
 			}
 		}
@@ -312,11 +627,13 @@ function AddConstraint( table, wordref )
 	else if( wordref.word.text.toUpperCase() === ( "FOREIGN" ) )
 	{
 		wordref.word = Next( wordref );
+		// optional KEY keywrod
 		if( wordref.word.text.toUpperCase() === ( "KEY" ) )
 		{
 			// next word is the type, skip that word too....
 			wordref.word = Next( wordref );
 		}
+		//table->constraints.constraint[table->constraints.count-1].flags.foreign_key = 1;
 	}
 	GrabKeyColumns( word, constraint.colnames );
 	if( wordref.word.text.toUpperCase() === ( "REFERENCES" ) )
